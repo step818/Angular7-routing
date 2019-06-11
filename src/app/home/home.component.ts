7ad0a167a1d045c1459899e14c017be4b9f3c2db
Stephen Trewick <stephent818@gmail.com>
2019-06-11 15:49:38 -0700

    data and click home css

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,13 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  h1Style: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
   firstClick() {
-    console.log('clicked');
+    this.h1Style = true;
   }
 
 }
